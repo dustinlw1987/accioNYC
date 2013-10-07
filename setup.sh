4e095088e8fde7e5a13d008be38dfd89b131e52b
dustinlw1987 <dlwilliams@deafgeeksdesign.com>
2013-10-06 21:29:57 -0700

    adding verbosity to certain commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 echo "Please give your permission to perform magic."
 sudo echo "Brewing your magical spells..."
+sudo rm -rfv /bin/accioNYC
 sudo ln -vs ./accioNYC /bin/accioNYC
 sudo echo "All done! Just call "accioNYC" at your convenience."
 sudo echo "Sectumsempra! \n Removing setup.sh and cleaning up."
-#sudo rm -rf ./setup.sh
+#sudo rm -rfv ./setup.sh
