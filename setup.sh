9fff032f458009f2a8ecac6fe26f238f216c1ce5
dustinlw1987 <dlwilliams@deafgeeksdesign.com>
2013-10-06 21:14:35 -0700

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Please give your permission to perform magic."
+sudo echo "Brewing your magical spells..."
+sudo ln -vs ./accioNYC /bin/accioNYC
+sudo echo "All done! Just call "accioNYC" at your convenience."
