54b0593d847d547ca1794a3852ab948b5df24858
dustinlw1987 <dlwilliams@deafgeeksdesign.com>
2013-10-06 21:17:17 -0700

    adding self-destruct to setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ echo "Please give your permission to perform magic."
 sudo echo "Brewing your magical spells..."
 sudo ln -vs ./accioNYC /bin/accioNYC
 sudo echo "All done! Just call "accioNYC" at your convenience."
+sudo echo "Sectumsempra! \n Removing setup.sh and cleaning up."
+#sudo rm -rf ./setup.sh
